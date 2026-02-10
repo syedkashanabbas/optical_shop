80ea7e3d6758dc3a8f37882ac3dad7059c81a254
Kashan Here.. <abbaskashan234@gmail.com>
2026-02-10 20:07:53 +0500

    rubi optical naming

--- resources/views/sales/pos.blade.php
+++ resources/views/sales/pos.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Posly - Ultimate Inventory Management System with POS</title>
+  <title>Rubi Optical - Ultimate Inventory Management System with POS</title>
 
   <!-- Favicon icon -->
   <link rel=icon href={{ asset('images/logo.svg') }}>
